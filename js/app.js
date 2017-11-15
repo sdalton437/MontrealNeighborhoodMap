d55290ff54478e45e81af91641ae43cbea97841e
Sam <daltonsam437@gmail.com>
2017-11-14 22:35:53 -0500

    Initial commit. Created html and js that displays google maps centered above Montreal with six locations marked on map. Js is organized with knockout framework.

---
+++ js/app.js
@@ -0,0 +1,47 @@
+var map;
+var placeMarkers = [];
+var markers = [];
+
+var locations = [
+  {title: 'Bar Darling', location: {lat: 45.518842, lng: -73.584071}},
+  {title: 'La Banquise', location: {lat: 45.5253521, lng: -73.5747681}},
+  {title: 'Divan Orange', location: {lat: 45.518224, lng: -73.582543}},
+  {title: 'Bar Bikteck St-Laurent', location: {lat: 45.5145979, lng: -73.5743797}},
+  {title: 'Mckibbins Irish Pub', location: {lat: 45.5134263, lng: -73.5712162}},
+  {title: 'Pitarifique', location: {lat: 45.5178508, lng: -73.5816863}}
+];
+
+var initMarkers = function(data){
+	var self = this;
+	this.title = data.title;
+	this.position = data.location;
+
+	this.marker = new google.maps.Marker({
+	  position: self.position,
+	  title: self.title,
+	  animation: google.maps.Animation.DROP,
+	  map: map
+	});
+}
+
+
+
+var ViewModel = function(){
+	
+	var self = this;
+	this.markers = [];
+	locations.forEach(function(data){
+		self.markers.push(new initMarkers(data));
+	});
+	
+    
+}
+
+function initMap(){
+	map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 45.5163646, lng: -73.5763104},
+      zoom: 14,
+      mapTypeControl: false
+    });
+	ko.applyBindings(new ViewModel());
+}
