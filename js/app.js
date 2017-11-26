a4f35ab3338f3fc20f44100985c03a4afb18e5f2
Sam <daltonsam437@gmail.com>
2017-11-26 10:40:46 -0500

    Fixed styling and added comments

--- js/app.js
+++ js/app.js
@@ -1,8 +1,4 @@
-var map;
-var placeMarkers = [];
-var largeInfowindow;
-
-
+//Define good locations in Montreal
 var locations = [
   {title: 'Bar Darling', location: {lat: 45.518842, lng: -73.584071}},
   {title: 'La Banquise', location: {lat: 45.5253521, lng: -73.5747681}},
@@ -12,6 +8,7 @@ var locations = [
   {title: 'Pitarifique', location: {lat: 45.5178508, lng: -73.5816863}}
 ];
 
+//Function to create markers, given location data from locations array
 var initMarkers = function(data){
 	var self = this;
 	this.title = data.title;
@@ -34,44 +31,46 @@ var initMarkers = function(data){
 var ViewModel = function(){
 	
 	var self = this;
+	//Create empty markers array
 	this.markers = [];
+	//Create listView ko observable array
 	this.listView = ko.observableArray();
 
 	//Populate markers array
 	locations.forEach(function(data){
 		self.markers.push(new initMarkers(data));
 	});
-	console.log(self.markers[0]);
+
 	//Populate list view array
 	locations.forEach(function(data){
 		self.listView.push(data);
 	});
 
-	this.listViewPopulate = function(data){
+	//Function that populates infowindow when listview item is clicked
+	this.listViewPopulateInfoWindow = function(data){
 		for(var j=0;j<self.markers.length;j++){
-			console.log("reacded");
-			console.log(data);
 			if(data.title == self.markers[j].title){
 				populateInfoWindow(self.markers[j].marker, largeInfowindow);
-				console.log(self.markers[j]);
 			}
 		}
 	}
 
-	console.log(self.listView()[0]);
-	this.textSearchPlaces = function(){
+	//Function that filters locations
+	this.filterPlaces = function(){
+		//Set all markers' map to null
 		for(var x = 0;x<self.markers.length;x++){
 			self.markers[x].marker.setMap(null);
 		};
 
+		//Remove all items from listView array
 		self.listView.removeAll();
-
+		//Get user query in lower case
 		var query = document.getElementById('filter-results-text').value.toLowerCase();
-		console.log(self.listView().length);
-		var counter = 0;
 
+		//Search through locations array
 		for(var i=0; i<locations.length; i++){
 			var index = locations[i].title.toLowerCase().search(query);
+			//if query is found in array, push that location to listView and set map
 			if(index != -1){
 				self.listView.push(locations[i]);
 				self.markers[i].marker.setMap(map);
@@ -87,117 +86,125 @@ var ViewModel = function(){
 var populateInfoWindow = function(marker, infowindow) {
     // Check to make sure the infowindow is not already opened on this marker.
     if (infowindow.marker != marker) {
-    	console.log(marker);
+    	//Set animation to bounce to alert user marker is selected
     	marker.setAnimation(google.maps.Animation.BOUNCE);
+    	//Set max width of info window
     	infowindow.setOptions({maxWidth:250});
-      	// Clear the infowindow content to give the streetview time to load.
-      	infowindow.setContent('');
       	infowindow.marker = marker;
-      	// Make sure the marker property is cleared if the infowindow is closed.
+      	// Make sure the marker property  and animation is cleared if the infowindow is closed.
       	infowindow.addListener('closeclick', function() {
         	infowindow.marker = null;
         	marker.setAnimation(null);
       	});
-      	var foursquare_client_id = "3TUHGAUUEZ4U2JDJ24CTJZ42JMQ4QOMD1MR2CAGKJ50ALLBD";
-		var foursquare_client_secret = "REBANX5C3T3EGVESNDDO5TWX21DKPRIUVUDMBWBXJAQC0QUT";
-		var clientParameters = "client_id=" + foursquare_client_id + "&client_secret=" + foursquare_client_secret;
+      	var foursquare_client_id = '3TUHGAUUEZ4U2JDJ24CTJZ42JMQ4QOMD1MR2CAGKJ50ALLBD';
+		var foursquare_client_secret = 'REBANX5C3T3EGVESNDDO5TWX21DKPRIUVUDMBWBXJAQC0QUT';
+		var clientParameters = 'client_id=' + foursquare_client_id + 
+			'&client_secret=' + foursquare_client_secret;
 		for(var i=0;i<locations.length;i++){
 			var locationToGenerate;
+			//Get location details to generate
 			if(marker.title == locations[i].title){
 				locationToGenerate = locations[i];
 			}
 		}
-			var latitude = locationToGenerate.location.lat;
-			var longitude = locationToGenerate.location.lng;
-			var nameSpaces = locationToGenerate.title;
-			var name = nameSpaces.split(' ').join('_');
-			var searchUrl = "https://api.foursquare.com/v2/venues/search?" + clientParameters + "&v=20130815&ll=" + latitude + "," + longitude + "&query=" + name;
-			console.log(searchUrl);
-			var venueId;
-			$.ajax({
-				url: searchUrl,
-				dataType: "json",
-				success: function(data){
-					var venue = data.response.venues;
-					var venueName = venue[0].name;
-					venueId = venue[0].id;
-					var infoUrl = "https://api.foursquare.com/v2/venues/" + venueId + "?" + clientParameters + "&v=20130815";
-					$.ajax({
-						url: infoUrl,
-						dataType : "json",
-						success: function(data){
-							console.log(infoUrl);
-							var innerHTML = "<div>"
-							var info = data.response.venue;
-							try{
-								var name = info.name;
-								innerHTML += '<h2 style="text-align:center;">' + name + "</h2>";
-							}
-							catch(err){
-								var name = "?";
-							}
-							try{
-								var photo = info.photos.groups[0].items[0].prefix + "250x200" + info.photos.groups[0].items[0].suffix;
-								innerHTML += "<img src=" + photo + ">";
-							}
-							catch(err){
-								var photo = "";
-							}
-							try{
-								var daysOpen = info.hours.timeframes[0].days;	
-								innerHTML += "<br>Open: " + daysOpen;
-							}
-							catch(err){
-								var daysOpen = "";
-							}
-							try{
-								var hours = info.hours.timeframes[0].open[0].renderedTime;
-								innerHTML += ", " + hours;
-							}
-							catch(err){
-								var hours = "";
-								
-							}
-							try{
-								var review = info.tips.groups[0].items[0].text;
-								innerHTML += '<br> Reviews: ' + '<div style="font-style:italic;padding-left: 5px;">' + '"' + review + '"</div>';
-							}
-							catch(err){
-								var review = "";
-							}
-							
-							try{
-								var user = info.tips.groups[0].items[0].user.firstName + " " + info.tips.groups[0].items[0].user.lastName;
-								innerHTML += '<div style="padding-left:10px;">- ' + user + "</div>";
-							}
-							catch(err){
-								var user = "";
-							}
-							
-							innerHTML += '</div>';
-							infowindow.setContent(innerHTML);
-							console.log(name);
-						},
-						error: function(){
-							alert("Foursquare API could not be loaded");
+		var latitude = locationToGenerate.location.lat;
+		var longitude = locationToGenerate.location.lng;
+		var nameSpaces = locationToGenerate.title;
+		//Remove spaces in name
+		var name = nameSpaces.split(' ').join('_');
+		var searchUrl = 'https://api.foursquare.com/v2/venues/search?' + clientParameters + 
+			'&v=20130815&ll=' + latitude + ',' + longitude + '&query=' + name;
+		var venueId;
+		$.ajax({
+			//Search Foursquare for venues with specified name and lat,lng
+			url: searchUrl,
+			dataType: 'json',
+			//If success, search fouursquare for details on location
+			success: function(data){
+				var venue = data.response.venues;
+				var venueName = venue[0].name;
+				venueId = venue[0].id;
+				var infoUrl = 'https://api.foursquare.com/v2/venues/' + venueId + 
+					'?' + clientParameters + '&v=20130815';
+				$.ajax({
+					//Search foursquare for details on location with received venueID 
+					url: infoUrl,
+					dataType : 'json',
+					success: function(data){
+						var innerHTML = "<div>"
+						var info = data.response.venue;
+						//Try to find name
+						try{
+							var name = info.name;
+							innerHTML += '<h2 style="text-align:center;">' + name + '</h2>';
 						}
-					});
-				},
-				error: function(){
-					alert("Foursquare API could not be loaded");
-				}
-			});
+						catch(err){
+						}
+						//Try to find photo
+						try{
+							var photo = info.photos.groups[0].items[0].prefix + 
+								'250x200' + info.photos.groups[0].items[0].suffix;
+							innerHTML += '<img src=' + photo + '>';
+						}
+						catch(err){
+						}
+						//Try to find days and hours the venue is open
+						try{
+							for(var i=0;i<7;i++){
+								var daysOpen = info.hours.timeframes[i].days;	
+								innerHTML += '<br>Open: ' + daysOpen;
+								var hours = info.hours.timeframes[i].open[i].renderedTime;
+								innerHTML += ', ' + hours;
+							}
+						}
+						catch(err){
+						}
+						
+						//Try to find a review
+						try{
+							var review = info.tips.groups[0].items[0].text;
+							innerHTML += '<br> Reviews: ' + '<div style="font-style:italic;padding-left: 5px;">' 
+								+ '"' + review + '"</div>';
+						}
+						catch(err){
+						}
+						//Try to find user that wrote the review
+						try{
+							var user = info.tips.groups[0].items[0].user.firstName + 
+								' ' + info.tips.groups[0].items[0].user.lastName;
+							innerHTML += '<div style="padding-left:10px;">- ' + user + '</div>';
+						}
+						catch(err){
+						}
+						
+						innerHTML += '<br><div style="font-style: italic;"> Data Provided by Foursquare </div></div>';
+						infowindow.setContent(innerHTML);
+					},
+					//If error, alert user
+					error: function(){
+						alert('Foursquare API could not be loaded');
+					}
+				});
+			},
+			//If error, alert user
+			error: function(){
+				alert('Foursquare API could not be loaded');
+			}
+		});
+		//Open the window on the marker
 		infowindow.open(map, marker);
 	}
 }
 	
-
+//Function to initiate the map
 function initMap(){
+	//Create new map
 	map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 45.5163646, lng: -73.5763104},
       zoom: 14,
       mapTypeControl: false
     });
+    //Create new infowindow
     largeInfowindow = new google.maps.InfoWindow();
 	ko.applyBindings(new ViewModel());
 	
